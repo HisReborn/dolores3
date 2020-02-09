818ce13e1028f9710449d329c95527e3f7b1e1bd
HisReborn <106546807+HisReborn@users.noreply.github.com>
2020-02-09 12:08:25 +0000

    usb: gadget: composite: enable BESL support

--- public_html/www/sendMessage.php
+++ public_html/www/sendMessage.php
@@ -17,4 +17,15 @@ TODO:
     </div>
     <div class="card-block">
       <div id="prev" class="m-b-15"></div>
-      <form method="post" class="form-inline" act
+      <form method="post" class="form-inline" action="message.php">
+        <div class="input-group w-80">
+          <input class="form-control" type="text" id="message" name="message" placeholder="Write your message here">
+          <a href="#" class="input-group-addon w-20" id="emojis" data-toggle="popover"> <img src="<?php echo IMG_PATH . "/smiley.png" ?>" alt="Emojis" height="18" width="19"></a>
+        </div>
+        <button class="btn btn-secondary" type="submit">Send</button>
+      </form>
+      <span id="bytes">0</span> bytes (<span id="transactions">1</span> transactions)
+      <!-- Emojis -->
+      <div id="emojisTable" class="emojisContainer">
+        <button type="button" class="close" aria-label="Close">
+          <span aria-hidden="true">
