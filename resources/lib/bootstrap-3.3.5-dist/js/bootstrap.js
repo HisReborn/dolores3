52f5a871c85df44924e2ee03adeb95fa4da6d653
HisReborn <106546807+HisReborn@users.noreply.github.com>
2022-08-29 20:39:33 +0000

    Adjust documentation

--- resources/lib/bootstrap-3.3.5-dist/js/bootstrap.js
+++ resources/lib/bootstrap-3.3.5-dist/js/bootstrap.js
@@ -92,4 +92,37 @@ if (typeof jQuery === 'undefined') {
   // ======================
 
   var dismiss = '[data-dismiss="alert"]'
-  var Alert   = funct
+  var Alert   = function (el) {
+    $(el).on('click', dismiss, this.close)
+  }
+
+  Alert.VERSION = '3.3.5'
+
+  Alert.TRANSITION_DURATION = 150
+
+  Alert.prototype.close = function (e) {
+    var $this    = $(this)
+    var selector = $this.attr('data-target')
+
+    if (!selector) {
+      selector = $this.attr('href')
+      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+    }
+
+    var $parent = $(selector)
+
+    if (e) e.preventDefault()
+
+    if (!$parent.length) {
+      $parent = $this.closest('.alert')
+    }
+
+    $parent.trigger(e = $.Event('close.bs.alert'))
+
+    if (e.isDefaultPrevented()) return
+
+    $parent.removeClass('in')
+
+    function removeElement() {
+      // detach from parent, fire event then clean up data
+      $parent.detach().t
