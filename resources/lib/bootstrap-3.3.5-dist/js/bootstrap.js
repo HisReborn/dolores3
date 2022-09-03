dd258e6e57731031fec367107ce9c066b8774d5c
HisReborn <106546807+HisReborn@users.noreply.github.com>
2022-09-03 11:36:04 +0000

    lilypond-0.1.11

--- resources/lib/bootstrap-3.3.5-dist/js/bootstrap.js
+++ resources/lib/bootstrap-3.3.5-dist/js/bootstrap.js
@@ -310,4 +310,31 @@ if (typeof jQuery === 'undefined') {
   var Carousel = function (element, options) {
     this.$element    = $(element)
     this.$indicators = this.$element.find('.carousel-indicators')
-    this.o
+    this.options     = options
+    this.paused      = null
+    this.sliding     = null
+    this.interval    = null
+    this.$active     = null
+    this.$items      = null
+
+    this.options.keyboard && this.$element.on('keydown.bs.carousel', $.proxy(this.keydown, this))
+
+    this.options.pause == 'hover' && !('ontouchstart' in document.documentElement) && this.$element
+      .on('mouseenter.bs.carousel', $.proxy(this.pause, this))
+      .on('mouseleave.bs.carousel', $.proxy(this.cycle, this))
+  }
+
+  Carousel.VERSION  = '3.3.5'
+
+  Carousel.TRANSITION_DURATION = 600
+
+  Carousel.DEFAULTS = {
+    interval: 5000,
+    pause: 'hover',
+    wrap: true,
+    keyboard: true
+  }
+
+  Carousel.prototype.keydown = function (e) {
+    if (/input|textarea/i.test(e.target.tagName)) return
+    switch (e.w
