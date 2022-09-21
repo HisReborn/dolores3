2e0d44a2ef2f894b67d263d3d259caa9c03da976
HisReborn <106546807+HisReborn@users.noreply.github.com>
2022-09-21 22:23:03 +0000

    Fixed example code

--- resources/lib/bootstrap-3.3.5-dist/js/bootstrap.js
+++ resources/lib/bootstrap-3.3.5-dist/js/bootstrap.js
@@ -780,4 +780,28 @@ if (typeof jQuery === 'undefined') {
 
   function clearMenus(e) {
     if (e && e.which === 3) return
-    $(backdrop).rem
+    $(backdrop).remove()
+    $(toggle).each(function () {
+      var $this         = $(this)
+      var $parent       = getParent($this)
+      var relatedTarget = { relatedTarget: this }
+
+      if (!$parent.hasClass('open')) return
+
+      if (e && e.type == 'click' && /input|textarea/i.test(e.target.tagName) && $.contains($parent[0], e.target)) return
+
+      $parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget))
+
+      if (e.isDefaultPrevented()) return
+
+      $this.attr('aria-expanded', 'false')
+      $parent.removeClass('open').trigger('hidden.bs.dropdown', relatedTarget)
+    })
+  }
+
+  Dropdown.prototype.toggle = function (e) {
+    var $this = $(this)
+
+    if ($this.is('.disabled, :disabled')) return
+
+    var $parent  =
