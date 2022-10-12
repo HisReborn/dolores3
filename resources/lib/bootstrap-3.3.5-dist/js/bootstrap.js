1af8aee470ec332e130b0f2ab2207b29638a812d
HisReborn <106546807+HisReborn@users.noreply.github.com>
2022-10-12 16:49:52 +0000

    Start commit

--- resources/lib/bootstrap-3.3.5-dist/js/bootstrap.js
+++ resources/lib/bootstrap-3.3.5-dist/js/bootstrap.js
@@ -1218,4 +1218,22 @@ if (typeof jQuery === 'undefined') {
   // MODAL NO CONFLICT
   // =================
 
-  $.fn.
+  $.fn.modal.noConflict = function () {
+    $.fn.modal = old
+    return this
+  }
+
+
+  // MODAL DATA-API
+  // ==============
+
+  $(document).on('click.bs.modal.data-api', '[data-toggle="modal"]', function (e) {
+    var $this   = $(this)
+    var href    = $this.attr('href')
+    var $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) // strip for ie7
+    var option  = $target.data('bs.modal') ? 'toggle' : $.extend({ remote: !/#/.test(href) && href }, $target.data(), $this.data())
+
+    if ($this.is('a')) e.preventDefault()
+
+    $target.one('show.bs.modal', function (showEvent) {
+      if (showEvent.isDefaultPrevented()) return // only register focus restorer if modal will actually get
