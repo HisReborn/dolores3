efd8d61a0b46c9271ab625831239e765a9da0482
HisReborn <106546807+HisReborn@users.noreply.github.com>
2023-03-29 10:01:50 +0000

    - Bugfix

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -15,4 +15,19 @@
     <link rel="stylesheet" href="<?php echo CSS_PATH . '/emoji.css' ?>">    
   </head>
   <body>
-    <!-
+    <!-- Navbar -->
+    <nav class="navbar navbar-light bg-faded navbar-fixed-top">
+      <a class="navbar-brand" href="<?php echo $localPath . '/index.php' ?>">
+        <img src="<?php echo IMG_PATH . '/logo.png' ?>" alt="<?php echo $config["title"]; ?>" class="navbar-brand" height="35px" width="35px">
+      </a>
+      <ul class="nav navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="<?php echo $localPath . '/index.php' ?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $localPath . '/sendMessage.php' ?>">Send message</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 2</a>
+        </li>
+        <li class
